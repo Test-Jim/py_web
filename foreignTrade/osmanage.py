8a80c27d27a13016362c7d727722fef392c6fe1e
jim <305540786@qq.com>
2018-04-13 14:51:58 +0800

    trade

---
+++ foreignTrade/osmanage.py
@@ -0,0 +1,8 @@
+import os,time
+os.system('python27 manage.py runserver 127.0.0.1:8000')
+# os.system('python27 manage.py startapp trade')
+# os.system('python manage.py makemigrations app')
+# time.sleep(1)
+# os.system('python manage.py migrate')
+
+
